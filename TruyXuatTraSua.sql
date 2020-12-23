74789de680d3f6b1d0520c54143ba86d749444d8
LhMinh2607 <68017090+LhMinh2607@users.noreply.github.com>
2020-12-23 09:12:21 +0700

    Update TruyXuatTraSua.sql

--- TruyXuatTraSua.sql
+++ TruyXuatTraSua.sql
@@ -1,4 +1,4 @@
-﻿USE MilkTeaShop
+USE MilkTeaShop
 GO
 
 CREATE PROC USP_GetAccountByUserName	
@@ -9,8 +9,8 @@ BEGIN
 END
 GO
 
-EXEC dbo.USP_GetAccountByUserName @username = N'kminh' -- nvarchar(50)
-GO
+--EXEC dbo.USP_GetAccountByUserName @username = N'kminh' -- nvarchar(50)
+--GO
 
 CREATE PROC USP_GetAccount	
 AS
@@ -19,8 +19,8 @@ BEGIN
 END
 GO
  
-EXEC dbo.USP_GetAccount
-GO
+--EXEC dbo.USP_GetAccount
+--GO
 
 CREATE PROC USP_Login	
 @userName NVARCHAR(100),@passWord NVARCHAR(100), @position NVARCHAR(100)
@@ -37,25 +37,30 @@ BEGIN
 END
 GO
 
-EXECUTE dbo.USP_GetCardList
+--EXECUTE dbo.USP_GetCardList
 
-SET IDENTITY_INSERT OrderCard ON
+--SET IDENTITY_INSERT OrderCard ON
 
-CREATE PROC CardUpdate(@n int)
+CREATE PROC CardUpdate(@i int)
 AS
 	BEGIN
-		DECLARE @i INT = 0;
-		DELETE OrderCard
-		DBCC CHECKIDENT(OrderCard, RESEED, 0)
-		WHILE @i<@n
-		BEGIN
-			INSERT dbo.OrderCard(CardName) VALUES (N'Thẻ số '+CAST(@i AS NVARCHAR(44)))
-			SET @i+=1
-		END
+		--DBCC CHECKIDENT(OrderCard, RESEED, 0)
+		INSERT dbo.OrderCard VALUES (@i, N'Thẻ số '+CAST(@i AS NVARCHAR(44)), 0)
+		SELECT* FROM dbo.OrderCard
 	END
-DROP PROC CardUpdate
+GO
+--DROP PROC CardUpdate
 
-EXEC CardUpdate 10
+--EXEC CardUpdate 31
+
+CREATE PROC CardRemove(@i int)
+AS
+	BEGIN
+		--DBCC CHECKIDENT(OrderCard, RESEED, 0)
+		DELETE dbo.OrderCard WHERE CardId=@i
+		SELECT* FROM dbo.OrderCard
+	END
+GO
 
 --UPDATE dbo.CardUpdate SET CardStatus = N'Đã có người' WHERE CardId =9
 
@@ -67,10 +72,10 @@ AS
 		FROM dbo.Account
 		WHERE Username=@Username AND PassWord=@pass
 	END
+GO
+--EXEC USP_GetPos N'hminh', N'minhdesigner' 
 
-EXEC USP_GetPos N'hminh', N'minhdesigner' 
-
-EXEC USP_GetPos N'kminh', N'1701yeu2409' 
+--EXEC USP_GetPos N'kminh', N'1701yeu2409' 
 
 CREATE PROC CheckAccount(@Username nvarchar(100), @pass nvarchar(100))
 AS
@@ -82,9 +87,10 @@ AS
 		IF(@Bool=0)
 			SELECT N'false'
 	END
-DROP PROC CheckAccount
-EXEC CheckAccount N'hminh', 'minhdesigner'
-EXEC CheckAccount N'hminh1', 'minhdesigner1'
+GO
+--DROP PROC CheckAccount
+--EXEC CheckAccount N'hminh', 'minhdesigner'
+--EXEC CheckAccount N'hminh1', 'minhdesigner1'
 
 
 CREATE PROC SelectCategoryNumber(@CategoryId nvarchar(10))
@@ -92,5 +98,103 @@ AS
 	BEGIN
 		SELECT COUNT(*) FROM Category WHERE (CategoryId = @CategoryId)
 	END
+GO
+--EXEC SelectCategoryNumber N'N0'
 
-EXEC SelectCategoryNumber N'N0'
+CREATE PROC SelectAllCategoryId
+AS
+	BEGIN
+		SELECT CategoryId FROM Category
+	END
+--EXEC SelectAllCategoryId
+GO
+
+CREATE PROC SelectCategoryName(@Cid varchar(10))
+AS
+	BEGIN
+		SELECT CategoryName FROM Category WHERE CategoryId=@Cid
+	END
+--EXEC SelectCategoryName 'ORG'
+GO
+
+/*CREATE PROC InsertBill(@CardId int)
+AS
+	BEGIN
+		INSERT dbo.Bill VALUES (CAST(@CardId AS VARCHAR(10)) + CAST(DatePart(Hour, GetDate()) AS varchar(10)) + CAST(DatePart(Minute, GetDate()) AS varchar(10)) + CAST(DatePart(SECOND, GetDate()) AS varchar(10)), 
+		@CardId, getDate(), 0, 0)
+		
+		SELECT* FROM dbo.Bill
+	END	
+GO*/
+
+CREATE PROC GetBillId(@CardId int)
+AS
+	BEGIN
+		--DBCC CHECKIDENT(BillDetail, RESEED, 1)
+		SELECT CAST(@CardId AS VARCHAR(10)) + CAST(DatePart(Hour, GetDate()) AS varchar(10)) + CAST(DatePart(Minute, GetDate()) AS varchar(10)) + CAST(DatePart(SECOND, GetDate()) AS varchar(10))
+	END	
+
+CREATE PROC GetBillDetailId(@BillId int)
+AS
+	BEGIN
+		--DBCC CHECKIDENT(BillDetail, RESEED, 1)
+		SELECT @BillId + CAST(DatePart(Minute, GetDate()) AS varchar(10)) + CAST(DatePart(SECOND, GetDate()) AS varchar(10))
+	END	
+--DROP PROC GetBillId
+--DBCC CHECKIDENT(OrderCard, RESEED, 0)
+--InsertBill 1
+
+CREATE PROC GetCurrentDate
+AS
+	BEGIN
+		SELECT GETDATE()
+	END
+
+CREATE PROC GetLatestBillId
+AS
+	BEGIN
+		SELECT TOP 1 *
+		FROM Bill
+		ORDER BY BillDate DESC
+	END
+
+SET IDENTITY_INSERT BillDetail Off
+
+CREATE PROC InsertBillDetail(@BillId varchar(10), @DrinkId varchar(10), @Number int, @Size varchar(10), @Topping varchar(250), @Ice varchar(10), @Sugar varchar(10), @BillDetailPrice int)
+AS
+	BEGIN
+		INSERT INTO BillDetail(BillId) VALUES(@BillId)
+		INSERT INTO BillDetail(DrinkId) VALUES(@DrinkId)
+		INSERT INTO BillDetail(Number) VALUES(@Number)
+		INSERT INTO BillDetail(Size) VALUES(@Size)
+		INSERT INTO BillDetail(Topping) VALUES(@Topping)
+		INSERT INTO BillDetail(Ice) VALUES(@Ice)
+		INSERT INTO BillDetail(Sugar) VALUES(@Sugar)
+		INSERT INTO BillDetail(BillDetailPrice) VALUES(@BillDetailPrice)
+	END
+--INSERT INTO BillDetail VALUES('2185853', 'TRADIT', 1, N'S (Nhỏ)', N'Trân châu vàng', '50%', '50%', 25000)
+
+CREATE PROC GetBillDetailByLatestBillId
+AS
+	BEGIN
+		SELECT *
+		FROM BillDetail 
+		WHERE BillId=(SELECT TOP 1 BillId FROM Bill ORDER BY BillDate DESC)
+	END
+
+
+CREATE TRIGGER tr_UpdateBill 
+ON BillDetail
+	FOR INSERT
+		AS
+			BEGIN
+				UPDATE Bill SET TotalPrice+=BillDetail.BillDetailPrice
+				FROM inserted, BillDetail
+			END
+--DROP TRIGGER tr_UpdateBill
+--INSERT INTO BillDetail VALUES('2185853', 'TRADIT', 1, N'S (Nhỏ)', N'Trân châu vàng', '50%', '50%', 25000)
+
+
+--INSERT INTO BillDetail VALUES('10212350', 'TRADIT', 1, N'S (Nhỏ)', N'Trân châu vàng', '50%', '50%', 25000)
+
+--DBCC CHECKIDENT(BillDetail, RESEED, 0)
